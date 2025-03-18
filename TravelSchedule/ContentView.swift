0e585f28c4cfd15873f047246f5bdc1043dba02b
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2025-03-18 18:37:01 +0300

    feat(openapi): Добавление API Яндекс Расписаний + сервис для получения Списка ближайших станций

--- TravelSchedule/ContentView.swift
+++ TravelSchedule/ContentView.swift
@@ -6,17 +6,55 @@
 //
 
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Get nearest stations") {
+                getNearestStations()
+            }
         }
         .padding()
     }
+}
+
+private func getNearestStations() {
+    let client = createClient()
+    guard let client else { return }
+    
+    let service = NearestStationsService(
+        client: client,
+        apikey: ServicesConstants.apikey
+    )
+    
+    Task {
+        let stations = try await service.getNearestStations(
+            lat: 50.440046,
+            lng: 40.4882367,
+            distance: 50
+        )
+        print(stations)
+    }
+}
+
+private func createClient() -> Client? {
+    var url: URL?
+    
+    do {
+        url = try Servers.Server1.url()
+    } catch {
+        print(String(describing: error))
+    }
+    
+    guard let url else { return nil }
+    
+    let client = Client(
+        serverURL: url,
+        transport: URLSessionTransport()
+    )
+    
+    return client
 }
 
 #Preview {
