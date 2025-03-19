476cb40869265d76535047b13a83fe231d880efd
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2025-03-19 20:58:20 +0300

    feat(openapi): Добавление методов для Бдижайшего города и Информации о перевозчике

--- TravelSchedule/ContentView.swift
+++ TravelSchedule/ContentView.swift
@@ -14,6 +14,14 @@ struct ContentView: View {
             Button("Get nearest stations") {
                 getNearestStations()
             }
+            
+            Button("Get nearest settlement") {
+                getNearestSettlement()
+            }
+            
+            Button("Get carriers") {
+                getCarriers()
+            }
         }
         .padding()
     }
@@ -29,12 +37,61 @@ private func getNearestStations() {
     )
     
     Task {
-        let stations = try await service.getNearestStations(
-            lat: 50.440046,
-            lng: 40.4882367,
-            distance: 50
-        )
-        print(stations)
+        do {
+            let stations = try await service.getNearestStations(
+                lat: 50.440046,
+                lng: 40.4882367,
+                distance: 50
+            )
+            print(stations)
+        } catch {
+            print("error response: \(error.localizedDescription)")
+        }
+    }
+}
+
+private func getNearestSettlement() {
+    let client = createClient()
+    guard let client else { return }
+    
+    let service = NearestSettlementService(
+        client: client,
+        apikey: ServicesConstants.apikey
+    )
+    
+    Task {
+        do {
+            let settlement = try await service.getNearestSettlement(
+                lat: 50.440046,
+                lng: 40.4882367,
+                distance: 50
+            )
+            print(settlement)
+        } catch {
+            print("error response: \(error.localizedDescription)")
+        }
+    }
+}
+
+private func getCarriers() {
+    let client = createClient()
+    guard let client else { return }
+    
+    let service = CarriersService(
+        client: client,
+        apikey: ServicesConstants.apikey
+    )
+    
+    Task {
+        do {
+            let carriers = try await service.getCarriers(
+                code: "TK",
+                system: "iata"
+            )
+            print(carriers)
+        } catch {
+            print("error response: \(error.localizedDescription)")
+        }
     }
 }
 
