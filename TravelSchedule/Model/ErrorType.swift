44c2b8996a9bc0ec222cae13e5160ea1a98688f1
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2025-03-31 20:36:16 +0300

    feat: Верстка экранов с ошибками(Ошибка сервера, Нет интернета)

---
+++ TravelSchedule/Model/ErrorType.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorType.swift
+//  TravelSchedule
+//
+//  Created by Anastasiia on 31.03.2025.
+//
+
+import Foundation
+
+enum ErrorType {
+    case internetConnectionError
+    case serverError
+}
---
+++ TravelSchedule/View/GenericErrorView.swift
@@ -0,0 +1,41 @@
+//
+//  GenericErrorView.swift
+//  TravelSchedule
+//
+//  Created by Anastasiia on 31.03.2025.
+//
+
+import SwiftUI
+
+struct GenericErrorView: View {
+    let imageName: String
+    let text: String
+    
+    init(error: ErrorType) {
+        switch error {
+        case .internetConnectionError:
+            self.imageName = "InternetError"
+            self.text = "Нет интернета"
+        case .serverError:
+            self.imageName = "ServerError"
+            self.text = "Ошибка сервера"
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            Color.ypWhite.ignoresSafeArea()
+            
+            VStack(alignment: .center, spacing: 16) {
+                Image(imageName)
+                Text(text)
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundStyle(.ypBlack)
+            }
+        }
+    }
+}
+
+#Preview {
+    GenericErrorView(error: .serverError)
+}
